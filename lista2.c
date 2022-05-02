f24ad976fba770a886040a33eadfcf5b02080fd5
IzabelyBrumR <103642004+IzabelyBrumR@users.noreply.github.com>
2022-05-02 10:35:53 -0300

    Update lista2.c

--- lista2.c
+++ lista2.c
@@ -19,3 +19,38 @@ int main(){
     printf("%d", numero);
 
 }
+
+#EXERCICIO 03
+
+#include <stdio.h>
+#include <conio.h>
+#include <math.h>
+
+main(){
+
+    char c;
+    scanf("%c", &c);
+
+    if(c == 'p'){
+
+        printf("Voce e paulista");
+
+        }
+
+    if(c == 'r'){
+
+        printf("Voce e carioca");
+
+    }
+
+    if(c == 'm'){
+
+        printf("Voce e mineiro");
+
+    }
+
+    if((c != 'p') && (c != 'r') && (c != 'm')){
+
+        printf("Outro estado");
+    }
+}
