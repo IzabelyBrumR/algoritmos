ac2f9df064ea6b97b61db3292f7216281ff6d7f6
IzabelyBrumR <103642004+IzabelyBrumR@users.noreply.github.com>
2022-07-08 14:57:50 -0300

    Update Exercicio7.c

--- for/Exercicio7.c
+++ for/Exercicio7.c
@@ -7,7 +7,7 @@ main(){
 char nome;
 float nota1, nota2, i, media_p, soma=0, media_t;
 
-for(i=0; i<3; i++){
+for(i=0; i<15; i++){
 
     printf("\nEntre com a inicial do seu nome:\n");
     scanf("\n%c", &nome);
@@ -18,18 +18,18 @@ for(i=0; i<3; i++){
     printf("Entre com a nota da sua segunda prova:\n");
     scanf("%f", &nota2);
 
-    soma = soma + (nota1 + nota2);
+    media_p = (nota1 + nota2) /2;
 
-    media_p = soma /2;
+    soma += (nota1 + nota2);
 
     printf("\nO seu nome e:%c", nome);
     printf("\nA nota da sua primeira prova: %f", nota1);
     printf("\nA nota da sua segunda prova: %f", nota2);
-    printf("\nA media apenas da sua nota: %f", media_p);
+    printf("\nA media apenas da sua nota: %f\n", media_p);
 }
 
-media_t = soma /3;
+media_t = soma /30;
 
-printf("\nA media de todos os alunos: %f", media_t);
+printf("\nA media de todos os alunos: %f\n", media_t);
 
 }
