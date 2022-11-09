1b4bfe125e1b0f6165a42265c57fa960889807ac
IzabelyBrumR <103642004+IzabelyBrumR@users.noreply.github.com>
2022-11-09 09:07:53 -0300

    Add files via upload

---
+++ função/ponteiros/Untitled1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <conio.h>
+
+main(){
+
+float matrix[50][50];
+float *p;
+int count;
+
+p = matrix[0];
+
+    for(count=0; count<2500; count++){
+
+        *p=0.0;
+          p++;
+    }
+     printf("%f", p);
+}
